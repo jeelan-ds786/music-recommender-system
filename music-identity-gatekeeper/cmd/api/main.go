a81fb43e7a4527c9958b620e3c14c778993dfa19
Fareedh Mouzamjah <97782415+jeelan-ds786@users.noreply.github.com>
2026-06-10 22:21:45 +0530

    mmm673 | Day 1 | GO SERVICE SCAFFOLD
    * mmm673 | Day 1 | redis intialization
    
    * mmm673 | workflow and lint fix
    
    * mmm673 | go version lint fix
    
    * mmm673 | issue fix
    
    * mmm673 | fixing the version

---
+++ music-identity-gatekeeper/cmd/api/main.go
@@ -0,0 +1,44 @@
+
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jeelan-ds786/music-recommender-system/music-identity-gatekeeper/internal/config"
+	"github.com/jeelan-ds786/music-recommender-system/music-identity-gatekeeper/internal/db"
+)
+
+func main() {
+
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	pgDB, err := db.ConnectPostgres(cfg.DB_URL)
+	if err != nil {
+		log.Fatalf("Could not connect to Postgres: %v", err)
+	}
+
+	defer func() {
+		if err := pgDB.Close(); err != nil {
+			log.Printf("Error closing Postgres connection: %v", err)
+		}
+	}()
+	fmt.Println("Successfully connected to Postgres!")
+
+	redisClient, err := db.ConnectRedis(cfg.REDIS_URL)
+	if err != nil {
+		log.Fatalf("Could not connect to Redis: %v", err)
+	}
+
+	defer func() {
+		if err := redisClient.Close(); err != nil {
+			log.Printf("Error closing Redis connection: %v", err)
+		}
+	}()
+	fmt.Println("Successfully connected to Redis!")
+
+	fmt.Printf("Service is live on port %s\n", cfg.PORT)
+}
